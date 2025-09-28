97333d60376b0306549f418b9a7ffc597cdd7c75
Sven <skbotnl@disroot.org>
2025-09-28 15:36:43 +0200

    Handle more events where blocks get broken/removed

--- src/main/kotlin/net/trueog/antiPieRayOG/Events.kt
+++ src/main/kotlin/net/trueog/antiPieRayOG/Events.kt
@@ -2,10 +2,13 @@ package net.trueog.antiPieRayOG
 
 import net.trueog.antiPieRayOG.BlockEntityHider.Companion.toBlockPosition
 import org.bukkit.Bukkit
+import org.bukkit.block.Block
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockBurnEvent
+import org.bukkit.event.block.BlockExplodeEvent
 import org.bukkit.event.player.PlayerChangedWorldEvent
 import org.bukkit.event.player.PlayerMoveEvent
 
@@ -23,22 +26,33 @@ class Events : Listener {
             )
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    fun onBlockBreak(event: BlockBreakEvent) {
-        val worldName = event.player.world.name
+    fun genericBlockBreakHandler(block: Block) {
+        val worldName = block.world.name
         if (worldName != "world" && worldName != "world_nether" && worldName != "world_the_end") return
 
         Bukkit.getScheduler()
             .runTaskLaterAsynchronously(
                 AntiPieRay.plugin,
                 Runnable {
-                    AntiPieRay.blockEntityHider.updateBlockVisibility(
-                        event.block.location.toBlockPosition(),
-                        event.block.world,
-                    )
+                    AntiPieRay.blockEntityHider.updateBlockVisibility(block.location.toBlockPosition(), block.world)
                 },
                 1L,
             )
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onBlockBreak(event: BlockBreakEvent) {
+        genericBlockBreakHandler(event.block)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onBlockBurn(event: BlockBurnEvent) {
+        genericBlockBreakHandler(event.block)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    fun onBlockExplode(event: BlockExplodeEvent) {
+        genericBlockBreakHandler(event.block)
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
